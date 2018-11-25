e57b9580b8a97dbbd8e7a6d6ce4ca665df844c89
Ron Silva <ron_silva@outlook.com>
2018-11-24 23:34:49 -0800

    Boolean Logic Samples

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+
+
+class Program
+{
+    static void Main()
+    {
+        //AND
+        //Console.WriteLine(true && false);
+        //Console.WriteLine(true && true);
+        //Console.WriteLine(false && false);
+
+        //OR
+        //Console.WriteLine(true || true);
+        //Console.WriteLine(true || false);
+        //Console.WriteLine(false || false);
+
+        //Equal
+        //Console.WriteLine(true == true);
+        //Console.WriteLine(true == false);
+        //Console.WriteLine(false == false);
+
+        //Not Equal
+        //Console.WriteLine(true != true);
+        //Console.WriteLine(true != false);
+        //Console.WriteLine(false != false);
+
+        // XOR Operator 
+        //Console.WriteLine(true ^ true);
+        //Console.WriteLine(true ^ false);
+        //Console.WriteLine(false ^ true);
+
+        Console.ReadLine();
+
+    }
+}
+
