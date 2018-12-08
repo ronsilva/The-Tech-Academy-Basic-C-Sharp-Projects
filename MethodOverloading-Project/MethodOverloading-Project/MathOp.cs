f8fcaa8d2134f268a8843d8eaadaa22ebb0629dd
Ron Silva <ron_silva@outlook.com>
2018-12-07 23:00:27 -0800

    Add Method Overloading - Project

---
+++ MethodOverloading-Project/MethodOverloading-Project/MathOp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading_Project
+{
+    public class MathOp
+    {
+        public int IntMathOp(int integer)
+        {
+            int result = integer + 7;
+            return result;
+        }
+        public int IntMathOp(decimal deci)
+        {
+            decimal dResult = deci * 7;
+            int result = Convert.ToInt32(dResult);
+            return result;
+        }
+        public int IntMathOp(string line)
+        {
+            int result = 0;
+            int tempResult = 0;
+            
+            try
+            {
+                int sToInt = Convert.ToInt32(line);
+                tempResult = sToInt / 4;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("*****************************");
+                Console.WriteLine(ex);
+                Console.WriteLine("*****************************");
+            }
+            finally
+            {
+                int number;
+                if (int.TryParse(line, out number))
+                {
+                    result = tempResult;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry! Couldn't do that.");
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ MethodOverloading-Project/MethodOverloading-Project/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading_Project
+{
+    class Program
+    {
+        static void Main()
+        {
+            //1.0 Create a class. DONE 
+            //1.1 In that class, create a method that will take in an integer, do a math operation to it and then return the answer as an integer. DONE
+            //2.0 In the Main() method of the console app, instantiate the class. DONE 
+            MathOp mo = new MathOp();
+            //2.1 and call the one method, passing in an integer, such as 12.
+            int result = mo.IntMathOp(12);
+            // 2.2 Then display the result to the screen.
+            Console.WriteLine(result);
+            //3. Add a second method to the class, with the same name, that will take in a decimal, do a different math operation to it and then return the answer as an integer. DONE
+            //4. In the Main() method of the console app, instantiate the class and call the second method, passing in a decimal. Display the result to the screen. DONE
+            MathOp mo2 = new MathOp();
+            int mo2Result = mo2.IntMathOp(12.50m);
+            Console.WriteLine(mo2Result);
+            //5. Add a third method to the class, with the same name, that will take in a string, convert it to an integer if possible, do a different math operation to it and then return the answer as an integer. DONE
+            //6. In the Main() method of the console app, instantiate the class and call the third method, passing in a string that equates to an integer.Display the result to the screen. DONE
+            MathOp mo3 = new MathOp();
+            int line = mo3.IntMathOp("20");
+            Console.WriteLine(line);
+            Console.ReadLine();
+        }
+    }
+}
