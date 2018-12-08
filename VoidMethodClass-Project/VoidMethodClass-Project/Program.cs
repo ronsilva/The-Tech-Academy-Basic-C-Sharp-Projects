3a49e7bb8ec9449505c1255cac4a0cf2b896d699
Ron Silva <ron_silva@outlook.com>
2018-12-07 16:55:24 -0800

    Create Void Method project

---
+++ VoidMethodClass-Project/VoidMethodClass-Project/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodClass_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VoidMethod instance1 = new VoidMethod();
+            instance1.DoACoupleOfThings(5, 60);
+            instance1.DoACoupleOfThings(num1: 5, num2: 60);
+        }
+    }
+}
---
+++ VoidMethodClass-Project/VoidMethodClass-Project/VoidMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodClass_Project
+{
+    public class VoidMethod
+    {
+        public void DoACoupleOfThings(int num1, int num2)
+        {
+            int total = num1 * 10;
+            Console.WriteLine(num2);
+        }
+    }
+}
