5a0e748f5f15ab88817c7d1e5f91fc2c8c6933bb
Ron Silva <ron_silva@outlook.com>
2018-12-07 17:02:39 -0800

    Add exercise text

--- VoidMethodClass-Project/VoidMethodClass-Project/Program.cs
+++ VoidMethodClass-Project/VoidMethodClass-Project/Program.cs
@@ -10,8 +10,13 @@ namespace VoidMethodClass_Project
     {
         static void Main(string[] args)
         {
+            //1.0 Create a class. In that class, create a void method that takes two integers as parameters. DONE
+            //1.1 Have the method do a math operation on the first integer and display the second integer to the screen. DONE
+            //2. In the Main() method of the console app, instantiate the class. DONE
             VoidMethod instance1 = new VoidMethod();
+            //3. Call the method in the class, passing in two numbers. DONE
             instance1.DoACoupleOfThings(5, 60);
+            //4. Call the method in the class, specifying the parameters by name. DONE
             instance1.DoACoupleOfThings(num1: 5, num2: 60);
         }
     }
