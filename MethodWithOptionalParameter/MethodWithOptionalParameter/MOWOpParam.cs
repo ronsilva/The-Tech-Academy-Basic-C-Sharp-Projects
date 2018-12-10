d204f8f369b42489bea43d5ab7e45f30dc6781ba
Ron Silva <ron_silva@outlook.com>
2018-12-10 11:53:47 -0800

    Add MWOpParam Project

---
+++ MethodWithOptionalParameter/MethodWithOptionalParameter/MOWOpParam.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodWithOptionalParameter
+{
+    public class MOWOpParam
+    {
+        public int MOMath(int num1, int num2 = 0)
+        {
+            int total = (num1 * num2) / num1;
+            return total;
+        }
+    }
+}
---
+++ MethodWithOptionalParameter/MethodWithOptionalParameter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodWithOptionalParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MOWOpParam mo = new MOWOpParam();
+            Console.WriteLine("Please enter a number: ");
+            int num1  = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int total = mo.MOMath(num1, num2);
+            Console.WriteLine("Your total is: {0}. which is also your second number: {1}", total, num2 );
+            Console.ReadLine();
+
+        }
+    }
+}
