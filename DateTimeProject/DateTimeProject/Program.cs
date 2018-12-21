821ca48f45db51ad100c9d8cea72d04cb97799f6
Ron Silva <ron_silva@outlook.com>
2018-12-21 02:50:03 -0800

    Add DateTimePRoject

---
+++ DateTimeProject/DateTimeProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DateTimeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Prints the current date and time to the console.
+            Console.WriteLine(DateTime.Now);
+            //2.Asks the user for a number.
+            Console.Write("Enter a number: ");
+            double dub = Convert.ToInt32(Console.ReadLine());
+            //3.Prints to the console the exact time it will be in X hours, X being the number the user entered in step 2.
+            Console.WriteLine(DateTime.Now.AddHours(dub));
+            Console.ReadLine();
+        }
+    }
+}
