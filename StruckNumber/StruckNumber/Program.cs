f9d24e27c35c91cb0e2ec86626603613ba282975
Ron Silva <ron_silva@outlook.com>
2018-12-20 19:47:42 -0800

    Add StruckNumber project

---
+++ StruckNumber/StruckNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StruckNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            //1.Create a struct called Number and give it the property "Amount" and have it be of data type decimal. DONE
+
+            //2. In the Main() method, create an object of data type Number and assign an amount to it.
+
+            Number num = new Number();
+            num.Amount = 5;
+
+            //3. Print this amount to the console.
+
+            Console.Write(num.Amount);
+            Console.ReadLine();
+        }
+    }
+}
