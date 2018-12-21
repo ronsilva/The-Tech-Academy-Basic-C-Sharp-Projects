ba573114bcb1e06869307c90ed055df5510c9470
Ron Silva <ron_silva@outlook.com>
2018-12-21 03:23:47 -0800

    Add ConstVarProject

---
+++ ConstVarProject/ConstVarProject/GoodNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVarProject
+{
+    class GoodNumber
+    {
+        private int _goodNumber;
+
+        public GoodNumber(string goodFor) : this(goodFor, 5) { }
+        public GoodNumber(string goodFor, int goodNumber)
+        {
+            GoodFor = goodFor;
+            Number = goodNumber;
+        }
+        public string GoodFor { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ ConstVarProject/ConstVarProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConstVarProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Create a const variable
+            const string myName = "Ron Silva";
+            Console.WriteLine("Hi! My name is {0}.", myName);
+            //3.Chain two constructors together. DONE
+            Console.Write("Please enter a plural noun: ");
+            //2.Create a variable using the keyword "var".
+            var answer = Console.ReadLine();
+            GoodNumber gN = new GoodNumber(answer);
+            Console.WriteLine("A good number of {0} is {1}.", gN.GoodFor, gN.Number);
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Thanks for playing!");
+            Console.ReadLine();
+        }
+    }
+}
