862ac41683c68fc6cdd1fda21983af4b0e62a2eb
Ron Silva <ron_silva@outlook.com>
2018-12-21 02:36:27 -0800

    Add TextLogging Project

---
+++ TextLoggingProject/TextLoggingProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace TextLoggingProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Ask a user for a number.
+            Console.Write("Please enter a number: ");
+            string userNumber = Console.ReadLine();
+            //2.Log that number to a text file.
+            File.WriteAllText(@"userNumber.txt", userNumber);
+            //3.Print the text file back to the user.
+            Console.WriteLine("You entered: {0}", File.ReadAllText(@"userNumber.txt"));
+            Console.ReadLine();
+        }    
+    }
+}
